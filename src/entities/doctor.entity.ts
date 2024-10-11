5247be28a03fd7511c4d890bae8756eb25604e0b
Debnath Mahapatra <debnathmahapatra740@gmail.com>
2024-10-11 21:39:48 +0530

    add access & refesh token

--- src/entities/doctor.entity.ts
+++ src/entities/doctor.entity.ts
@@ -1,25 +1,37 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { Document, Types } from 'mongoose';
 
 @Schema({ timestamps: true })
 export class DoctorEntity extends Document {
-  @Prop({ unique: true, required: true })
+  @Prop({
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true,
+    index: true,
+  })
   name: string;
 
-  @Prop({ required: true })
+  @Prop({ 
+    required: true, 
+    unique: true, 
+    lowercase: true, 
+    trim: true })
   email: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, lowercase: true, trim: true })
   speciality: string;
 
   @Prop({ required: true })
   experience: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, trim: true })
   about: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, trim: true })
   fees: string;
 
   @Prop({ select: false, required: true })
@@ -33,6 +45,9 @@ export class DoctorEntity extends Document {
 
   @Prop({ default: true })
   isActive: boolean;
+
+  @Prop({ required: true })
+  refreshToken: string;
 
   _id: Types.ObjectId;
 }
@@ -51,3 +66,59 @@ DoctorEntitySchema.pre<DoctorEntity>('save', async function (next) {
     next(error);
   }
 });
+
+DoctorEntitySchema.methods.isPasswordCorrect = async function (
+  password: string,
+) {
+  try {
+    return await compare(password, this.password);
+  } catch (error) {
+    console.error('Error during password comparison:', error);
+    throw new HttpException(
+      'An error occurred while verifying the password. Please try again.',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+};
+
+DoctorEntitySchema.methods.generateAccessToken = function () {
+  try {
+    return jwt.sign(
+      {
+        _id: this._id,
+        email: this.email,
+        name: this.name,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      },
+    );
+  } catch (error) {
+    console.error('Error generating access token:', error);
+    throw new HttpException(
+      'Failed to generate access token.',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+};
+
+DoctorEntitySchema.methods.generateRefreshToken = function () {
+  try {
+    return jwt.sign(
+      {
+        _id: this._id,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+      },
+    );
+  } catch (error) {
+    console.error('Error generating refresh token:', error);
+    throw new HttpException(
+      'Failed to generate refresh token.',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+};
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,22 +1,34 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 import { Document, Types } from 'mongoose';
+import jwt from 'jsonwebtoken';
 
 @Schema({ timestamps: true })
 export class UserEntity extends Document {
-  @Prop({ unique: true, required: true })
+  @Prop({
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true,
+    index: true,
+  })
   name: string;
 
-  @Prop({ required: true })
+  @Prop({ 
+    required: true, 
+    unique: true, 
+    lowercase: true, 
+    trim: true })
   email: string;
 
-  @Prop({ required: false })
+  @Prop({ required: false, trim: true })
   phone?: string;
 
-  @Prop({ required: false })
+  @Prop({ required: false, trim: true })
   address?: string;
 
-  @Prop({ required: false, enum: ['Male', 'Female', 'Others'] })
+  @Prop({ required: false, enum: ['male', 'female', 'others'], lowercase: true, trim: true })
   gender?: string;
 
   @Prop({ required: false })
@@ -33,6 +45,9 @@ export class UserEntity extends Document {
 
   @Prop({ default: true })
   isActive: boolean;
+
+  @Prop({ required: true })
+  refreshToken: string;
 
   _id: Types.ObjectId;
 }
@@ -51,3 +66,57 @@ UserEntitySchema.pre<UserEntity>('save', async function (next) {
     next(error);
   }
 });
+
+UserEntitySchema.methods.isPasswordCorrect = async function (password: string) {
+  try {
+    return await compare(password, this.password);
+  } catch (error) {
+    console.error('Error during password comparison:', error);
+    throw new HttpException(
+      'An error occurred while verifying the password. Please try again.',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+};
+
+UserEntitySchema.methods.generateAccessToken = function () {
+  try {
+    return jwt.sign(
+      {
+        _id: this._id,
+        email: this.email,
+        name: this.name,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      },
+    );
+  } catch (error) {
+    console.error('Error generating access token:', error);
+    throw new HttpException(
+      'Failed to generate access token.',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+};
+
+UserEntitySchema.methods.generateRefreshToken = function () {
+  try {
+    return jwt.sign(
+      {
+        _id: this._id,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+      },
+    );
+  } catch (error) {
+    console.error('Error generating refresh token:', error);
+    throw new HttpException(
+      'Failed to generate refresh token.',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+};
