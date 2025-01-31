339c1efb5da2f2455a0523aee7d226b19782bf6e
Debnath Mahapatra <debnathmahapatra740@gmail.com>
2025-01-31 15:55:36 +0530

    config CORS

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://bookify-beta.vercel.app/', 'http://localhost:3000'],
+    origin: 'https://bookify-beta.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   });
